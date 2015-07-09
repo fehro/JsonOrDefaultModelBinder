aa29f133afb36bd2ebee6bfc28afeb3601fd34f8
unknown <OliverF@DSC2822.dsc.wa.gov.au>
2015-07-09 16:41:44 +0800

    Initial commit

---
+++ JsonOrDefaultModelBinder.vb
@@ -0,0 +1,65 @@
+﻿Imports System.ComponentModel
+
+Public Class JsonOrDefaultModelBinder
+    Inherits DefaultModelBinder
+
+#Region " Overriden Methods "
+
+    ''' <summary>
+    ''' Overridden GetPropertyValue Function.
+    ''' </summary>
+    Protected Overrides Function GetPropertyValue(ByVal controllerContext As ControllerContext, ByVal bindingContext As ModelBindingContext, ByVal propertyDescriptor As PropertyDescriptor, ByVal propertyBinder As IModelBinder) As Object
+
+        'Try to get the value as a JSON model.
+        Dim value = BindJsonModel(controllerContext, bindingContext)
+
+        If (value Is Nothing) Then
+
+            'No value was returned from the BindJsonModel function. So try to get it the default way.
+            value = propertyBinder.BindModel(controllerContext, bindingContext)
+
+        End If
+
+        If bindingContext.ModelMetadata.ConvertEmptyStringToNull AndAlso Equals(value, [String].Empty) Then
+            Return Nothing
+        End If
+
+        Return value
+
+    End Function
+
+#End Region
+
+#Region " Private Methods "
+
+    ''' <summary>
+    ''' Attempt to bind the JSON data to the model.
+    ''' </summary>
+    Private Shared Function BindJsonModel(ByVal controllerContext As ControllerContext, ByVal bindingContext As ModelBindingContext)
+
+        If (bindingContext.ModelType.IsPrimitive OrElse bindingContext.ModelType.IsValueType OrElse bindingContext.ModelType Is GetType(String)) Then
+
+            'Property is not a complex type so do not attempt to deserialize.
+            Return Nothing
+
+        End If
+
+        Try
+
+            Dim serializedValue = controllerContext.HttpContext.Request(bindingContext.ModelName)
+
+            'Attempt to deserialize the value.
+            Return Newtonsoft.Json.JsonConvert.DeserializeObject(serializedValue, bindingContext.ModelType)
+
+        Catch ex As Exception
+
+            'Exception. So just return nothing and the calling function will perform further processing.
+            Return Nothing
+
+        End Try
+
+    End Function
+
+#End Region
+
+End Class
--- README.md
+++ README.md
@@ -1,2 +1,62 @@
 # JsonOrDefaultModelBinder
 A replacement MVC model binder that allows properties to be in serialized JSON form allowing Angular / Knockout whatever you want to manage segments of your MVC view's model.
+==============
+
+I apologise for this being all in VB.net. Also the example below is just a simple guideline. I know it is not how you should ng :)
+
+Usage
+--------------
+In the Application_Start of your Global.asax file simply add the line below to change your default model binder.
+
+    ModelBinders.Binders.DefaultBinder = New JsonOrDefaultModelBinder()
+
+Example
+--------------
+Assume you have two models as below.
+
+    Public Class PostcodeRange
+	
+	    Public Property FromPostcode As Integer
+		
+		Public Property ToPostcode As Integer
+		
+	End Class
+
+    Public Class PostcodesZone
+	
+	    Public Property Code As String
+		
+		Public Property PostcodeRanges As List(Of PostcodeRange)
+		
+	End Class
+	
+If you wanted to say handle the adding / removal of the PostcodeRanges list via Angular but the Code property in your Razor view then this can be done as below.
+
+**Razor Template**
+
+    @Using (Html.BeginForm())
+
+		@Html.TextBoxFor(Function(x) x.Code)
+	
+		<div ng-app ng-init="postcodeRanges = @Newtonsoft.Json.JsonConvert.SerializeObject(Model.PostcodeRanges)">
+		
+			<!-- Put your Angular logic here -->
+
+    		<input type="hidden" name="PostcodeRanges" value="{{postcodeRanges}}" />
+
+		</div>
+		
+		<input type="submit" value="Save" />
+	
+	End Using
+	
+**View Model**
+
+	<HttpPost>
+	Function Edit(model As PostcodesZone) As ActionResult
+	
+		'Do your saving here.
+
+		Return View("Edit", model)
+
+	End Function
